ef61654867ef3ae9296cc07244d20c1d14709566
johnbosco91 <johnboscoadam@gmail.com>
2021-12-15 09:45:51 +0300

    first commit

--- src/main/java/com/company/name/DefaultOrchestrator.java
+++ src/main/java/com/company/name/DefaultOrchestrator.java
@@ -4,12 +4,11 @@ import akka.actor.ActorSelection;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
-import org.apache.http.HttpStatus;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openhim.mediator.engine.MediatorConfig;
-import org.openhim.mediator.engine.messages.FinishRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPResponse;
-import org.json.JSONObject;
 
 
 public class DefaultOrchestrator extends UntypedActor {
@@ -26,12 +25,32 @@ public class DefaultOrchestrator extends UntypedActor {
     public void onReceive(Object msg) throws Exception {
         if (msg instanceof MediatorHTTPRequest) {
             String body = ((MediatorHTTPRequest) msg).getBody();
-             JSONObject jsonObject = new JSONObject(body);
+            JSONObject jsonObject = new JSONObject(body);
 
+            JSONObject orgUnit = new JSONObject();
+            String coordinates = "[" + jsonObject.get("Latitude") + "," + jsonObject.get("Longitude") + "]";
+            boolean isActive;
+            isActive = jsonObject.getString("OperatingStatus").equals("Operating");
 
-//            JSONObject Fac_IDNumber = new JSONObject(body.getJSONObject("HfrConfigs"));
-            System.out.println(jsonObject.get("Fac_IDNumber"));
-
+            orgUnit.put("code", jsonObject.get("FacilityTypeGroupCode"));
+            JSONArray OrgUnits = new JSONArray();
+            OrgUnits.put(orgUnit);
+            orgUnit.put("code", jsonObject.get("FacilityTypeCode"));
+            OrgUnits.put(orgUnit);
+            orgUnit.put("code", jsonObject.get("Ownership"));
+            OrgUnits.put(orgUnit);
+            orgUnit.put("code", jsonObject.get("OwnershipGroupCode"));
+            OrgUnits.put(orgUnit);
+            String jsonString = new JSONObject()
+                    .put("name", jsonObject.get("Name"))
+                    .put("code", jsonObject.get("Fac_IDNumber"))
+                    .put("shortName", jsonObject.get("Comm_FacName"))
+                    .put("openingDate", jsonObject.get("OpenedDate"))
+                    .put("coordinates", coordinates)
+                    .put("active", isActive)
+                    .put("parent", new JSONObject().put("code", jsonObject.get("Council_Code")))
+                    .put("organisationUnitGroups", OrgUnits)
+                    .toString();
             JSONObject HFRConfigs = new JSONObject(config.getDynamicConfig()).getJSONObject("HfrConfigs");
             String hfrUri = HFRConfigs.getString("url");
             MediatorHTTPRequest hfrRequest = new MediatorHTTPRequest(
@@ -39,49 +58,15 @@ public class DefaultOrchestrator extends UntypedActor {
                     getSelf(),
                     "Sending Request to",
                     "POST",
-                    hfrUri);
-
-            String jsonString = new JSONObject()
-                    .put("name", "Hello World!")
-                    .put("JSON2", "Hello my World!")
-                    .put("JSON3", new JSONObject().put("key1", "value1"))
-                    .toString();
-
-//            {
-//                "name": "jamatini Health Center",
-//                    "code": "120321-5",
-//                    "shortName": "jamatini",
-//                    "openingDate": "2021-04-08",
-//                    "coordinates": "[-6.721521,39.2428263]",
-//                    "active": true,
-//                    "parent": {
-//                "code": "TZ.CL.DO.DO.5"
-//            },
-//                "organisationUnitGroups": [
-//                {
-//                    "code": "HLCTR"
-//                },
-//                {
-//                    "code": "HLCTR"
-//                },
-//                {
-//                    "code": "LGA"
-//                },
-//                {
-//                    "code": "Publ"
-//                }
-//    ]
-//            }
-
-            System.out.println(jsonString);
+                    hfrUri, jsonString, null, null, null);
             //Getting instance of the http connector
             ActorSelection httpConnector = getContext().actorSelection(config.userPathFor("http-connector"));
 
             //firing request
-//            httpConnector.tell(hfrRequest, getSelf());
+            httpConnector.tell(hfrRequest, getSelf());
 
-            httpConnector.tell(jsonString, getSelf());
-        }else if(msg instanceof MediatorHTTPResponse){
+//            httpConnector.tell(jsonString, getSelf());
+        } else if (msg instanceof MediatorHTTPResponse) {
             ((MediatorHTTPResponse) msg).getOriginalRequest().getRequestHandler().tell(((MediatorHTTPResponse) msg).toFinishRequest(), getSelf());
         } else {
             unhandled(msg);
